b5b757d9d4680b62eb666f6c620b5836f627b247
Printice Lawton <printicelaw10@gmail.com>
2021-07-15 16:47:07 -0400

    category model

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,18 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Category extends Model {}
-
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -18,3 +24,4 @@ Category.init(
 );
 
 module.exports = Category;
+
